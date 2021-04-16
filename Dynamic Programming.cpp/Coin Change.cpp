c4afb3a4706ce248bb5688c8f9f4b4fb9df92bf2
ravi2615 <67025166+ravi2615@users.noreply.github.com>
2021-04-16 01:31:19 -0700

    new

--- Dynamic Programming.cpp/Coin Change.cpp
+++ Dynamic Programming.cpp/Coin Change.cpp
@@ -10,14 +10,12 @@ class Solution
     {
        
         //code here.
-        int dp[m+1][n+1];
+        long long int dp[m+1][n+1];
         for(int i=0;i<m+1;i++)
-        for(int j=0;j<n+1;j++){
-            if(i==0)
-            dp[i][j]=0;
-            if(j==0)
-            dp[i][j]=1;
-            
+        dp[i][0]=1;
+        
+        for(int j=1;j<n+1;j++){
+            dp[0][j]=0;
         }
         for(int i=1;i<m+1;i++)
         for(int j=1;j<n+1;j++){
