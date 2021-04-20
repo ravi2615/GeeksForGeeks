79b73becc55f9e016fc3bb26c36609a4aeb1261c
ravi2615 <67025166+ravi2615@users.noreply.github.com>
2021-04-19 23:30:21 -0700

    added new

--- Dynamic Programming.cpp/Count ways to N'th Stair(Order does not matter) .cpp
+++ Dynamic Programming.cpp/Count ways to N'th Stair(Order does not matter) .cpp
@@ -23,6 +23,9 @@ class Solution
             dp[i]=(dp[i-2]+1);
         }
         
+        // output
+        // 0 1 2 3 4 5 6 7 8 9 10
+        // 0 1 2 2 3 3 4 4 5 5 6
         return dp[m];
     }
 };
